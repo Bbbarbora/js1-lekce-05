59ba2db9eef1313d55a69eb1ce3afe0f913d0515
Barbora Lomenova <barbora.lomenova@gmail.com>
2025-02-24 20:04:32 +0100

    2 cast

--- index.js
+++ index.js
@@ -12,3 +12,19 @@ cardElement.classList.add('active');
 const imgEl = document.querySelector('.card img');
 imgEl.src = 'images/pes.jpg';
 imgEl.alt = 'nejhezci pejsanek na svete';
+
+const product1 = {
+    name: 'Lední brusle',
+    price: 1259,
+    description:
+      'Lední brusle dámské vhodné pro rekreační bruslaře, nůž: klasická svařovaná brusle, nerezová ocel',
+    color: 'white',
+  };
+
+const card3El = document.querySelector('card:nth-child(3)');
+
+card3El.innerHTML = `
+<h2>${product1.name} </h2>
+<p>
+
+`
